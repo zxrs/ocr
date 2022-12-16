5d1494a4347b45d66bd29c5e220e54fb4897e0b9
benki <tanashin@bj.wakwak.com>
2022-12-16 10:36:38 +0900

    avoid memory allocation

--- src/ocr.rs
+++ src/ocr.rs
@@ -1,3 +1,4 @@
+use super::BUF_SIZE;
 use anyhow::{ensure, Result};
 use std::io::{Cursor, Write};
 use std::ptr;
@@ -9,7 +10,13 @@ use windows::{
     Win32::System::WinRT::IMemoryBufferByteAccess,
 };
 
-pub fn scan(width: i32, height: i32, bytes_per_pixel: usize, bgr: Vec<u8>) -> Result<Vec<u16>> {
+pub fn scan(
+    width: i32,
+    height: i32,
+    bytes_per_pixel: usize,
+    bgr: Vec<u8>,
+    buf: &mut [u8],
+) -> Result<usize> {
     ensure!(bgr.len() > bytes_per_pixel, "no data");
 
     let bmp = SoftwareBitmap::Create(BitmapPixelFormat::Bgra8, width, height)?;
@@ -35,8 +42,7 @@ pub fn scan(width: i32, height: i32, bytes_per_pixel: usize, bgr: Vec<u8>) -> Re
 
     let engine = OcrEngine::TryCreateFromUserProfileLanguages()?;
 
-    let mut buf = [0u8; 4096];
-    let mut cur = Cursor::new(buf.as_mut_slice());
+    let mut cur = Cursor::new(buf);
     engine
         .RecognizeAsync(&bmp)?
         .get()?
@@ -45,7 +51,8 @@ pub fn scan(width: i32, height: i32, bytes_per_pixel: usize, bgr: Vec<u8>) -> Re
         .try_for_each(|line| -> Result<()> {
             line.Text()?
                 .as_wide()
-                .split(|num| num == &32)
+                // split by whitespace
+                .split(|num| num == &0x0020)
                 .try_for_each(|data| -> Result<()> {
                     let data = unsafe {
                         slice::from_raw_parts(data.as_ptr() as *const u8, data.len() * 2)
@@ -54,16 +61,26 @@ pub fn scan(width: i32, height: i32, bytes_per_pixel: usize, bgr: Vec<u8>) -> Re
                     if data.chunks(2).all(|n| n[0] < 0x80 && n[1] == 0) {
                         let pos = cur.position() as usize;
                         let r = cur.get_ref();
-                        // if the previous 4 bytes are "\r\n", do not insert a space.
-                        if pos > 3 && (*r)[pos - 4..pos] != [0x0d, 0x00, 0x0a, 0x00] {
-                            let _ = cur.write(&[32, 0])?;
+                        // if the previous 4 bytes are "\r\n" or 2bytes are " ", do not insert a space.
+                        if pos > 3
+                            && ((*r)[pos - 4..pos] != [0x0d, 0x00, 0x0a, 0x00]
+                                && (*r)[pos - 2..pos] != [0x20, 0x00])
+                        {
+                            let _ = cur.write(&[0x20, 0x00])?;
                         }
                         let _ = cur.write(data)?;
+                        let _ = cur.write(&[0x20, 0x00])?;
                     } else {
                         let _ = cur.write(data)?;
                     }
                     Ok(())
                 })?;
+            // if the last 2bytes are " ", remove it.
+            let pos = cur.position() as usize;
+            let r = cur.get_ref();
+            if pos > 2 && (*r)[pos - 2..pos] == [0x20, 0x00] {
+                cur.set_position(pos as u64 - 2);
+            }
             // add "\r\n"
             let _ = cur.write(&[0x0d, 0x00, 0x0a, 0x00])?;
             Ok(())
@@ -71,7 +88,11 @@ pub fn scan(width: i32, height: i32, bytes_per_pixel: usize, bgr: Vec<u8>) -> Re
     // null termination.
     let _ = cur.write(&[0, 0])?;
 
-    let len = cur.position() as usize / 2;
-    let result = unsafe { slice::from_raw_parts(&buf[..] as *const [u8] as *const u16, len) };
-    Ok(result.to_owned())
+    // the last 2 bytes of buffer should be null terminatation.
+    if cur.position() as usize > BUF_SIZE - 2 {
+        cur.set_position((BUF_SIZE - 2) as u64);
+        let _ = cur.write(&[0, 0])?;
+    }
+
+    Ok(cur.position() as usize)
 }
